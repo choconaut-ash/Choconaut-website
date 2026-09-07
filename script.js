3b258a803de6d38a4898ffd85e106c7e1685d057
choconaut-ash <omakase.enterprises@gmail.com>
2026-09-08 01:20:26 +0530

    Change Content-Type header to text/plain

--- script.js
+++ script.js
@@ -160,7 +160,7 @@ async function logOrderToSheet(statusType, overrideAddress = null) {
     try {
         await fetch(SCRIPT_URL, {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            headers: { 'Content-Type': 'text/plain;charset=utf-8' },
             body: JSON.stringify(payload)
         });
     } catch (e) {
